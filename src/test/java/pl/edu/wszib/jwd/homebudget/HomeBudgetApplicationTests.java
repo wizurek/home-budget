82f537b88f18ed9778642157061d1ca1f0195876
wizurek <wizurek@student.wszib.edu.pl>
2020-05-16 23:29:32 +0200

    Initial commit

---
+++ src/test/java/pl/edu/wszib/jwd/homebudget/HomeBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.jwd.homebudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeBudgetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
